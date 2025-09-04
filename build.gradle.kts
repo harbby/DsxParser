31096a418b6d0bdb5086266e6d556fb6f5c93d6b
ideal <ideal.hepeng@gmail.com>
2025-09-04 18:56:41 +0800

    upgrade dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,16 @@ plugins {
     id("antlr")
     signing
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.1.0"
+    id("com.github.ben-manes.versions") version "0.52.0"  // ./gradlew dependencyUpdates
 }
 
 group = "com.github.harbby"
 version = "1.0.8-SNAPSHOT" // SNAPSHOT
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -20,11 +21,10 @@ repositories {
 }
 
 object versions {
-    val gadtry = "1.10.7"
-    val junit = "5.10.0"
-    val antlr4 = "4.13.1"
-    val slf4j = "2.0.13"
-    val log4j2 = "2.23.1"
+    val gadtry = "1.10.8"
+    val junit = "5.13.4"
+    val antlr4 = "4.13.2"
+    val slf4j = "2.0.17"
 }
 
 dependencies {
@@ -34,7 +34,8 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:${versions.junit}"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.apache.logging.log4j:log4j-slf4j2-impl:${versions.log4j2}")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.slf4j:slf4j-log4j12:${versions.slf4j}")
 }
 
 tasks.test {
@@ -92,8 +93,6 @@ tasks.register<Jar>("javadocJar") {
     tasks.javadoc.orNull?.isFailOnError = false
 }
 //--- gradle clean build publish
-// export https_proxy=http://127.0.0.1:prot
-// export http_proxy=http://127.0.0.1:prot
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
